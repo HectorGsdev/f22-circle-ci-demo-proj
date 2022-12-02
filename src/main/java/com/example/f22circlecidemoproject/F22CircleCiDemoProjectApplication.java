eb380938b7d4748e6446a9c75424b1093447ead5
Hector Gonzalez <HectorGsdev@gmail.com>
2022-12-01 17:58:11 -0800

    Added initial files.

---
+++ src/main/java/com/example/f22circlecidemoproject/F22CircleCiDemoProjectApplication.java
@@ -0,0 +1,15 @@
+package com.example.f22circlecidemoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F22CircleCiDemoProjectApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(F22CircleCiDemoProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/f22circlecidemoproject/F22CircleCiDemoProjectApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.f22circlecidemoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F22CircleCiDemoProjectApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
